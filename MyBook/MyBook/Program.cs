07b86be538efeaa5284e98e73b29b97363ef8224
KFJ <KFJ@DESKTOP-TO6JOC7>
2020-07-03 19:36:53 +0800

    1

--- MyBook/MyBook/Program.cs
+++ MyBook/MyBook/Program.cs
@@ -16,7 +16,7 @@ namespace MyBook
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
